33a2cac8b8b6be9a962ec9843c838f8dc3af5900
WoeromProg <nikita174trofimov@gmail.com>
2023-05-26 16:06:40 +0500

    mod11

--- mod11/task3.py
+++ mod11/task3.py
@@ -12,27 +12,22 @@ logger = logging.getLogger(__name__)
 
 class Director(threading.Thread):
     def __init__(self, semaphore: threading.Semaphore):
-        super().__init__()
-        self.sem = semaphore
-        self.max_tickets = TICKETS_MAX
-        self.tickets_added = 0
+        super(Director, self).__init__()
+        self.lock = semaphore
         logger.info('Director started work')
 
     def run(self):
-        global TOTAL_TICKETS
-        global TICKETS_MAX
-        is_running = True
-        while is_running:
-            if TOTAL_TICKETS == 4:
-                with self.sem:
-                    tickets_to_print = min(TICKETS_MAX, 10 - TOTAL_TICKETS)
+        global TOTAL_TICKETS, TICKETS_MAX
+        while TICKETS_MAX:
+            if TOTAL_TICKETS <= 4:
+                with self.lock:
+                    tickets_to_print = 10 - (TOTAL_TICKETS % 10)
+                    if tickets_to_print > TICKETS_MAX:
+                        tickets_to_print = TICKETS_MAX
                     TOTAL_TICKETS += tickets_to_print
                     TICKETS_MAX -= tickets_to_print
-                    self.tickets_added += tickets_to_print
-                    logger.info(f'Director added {tickets_to_print} tickets; {TICKETS_MAX} left')
-                    if TICKETS_MAX == 0:
-                        is_running = False
-        logger.info(f'Director printed {self.tickets_added} tickets')
+                    logger.info(f'Director put {tickets_to_print} tickets')
+        logger.info(f'Director printed tickets, stop work')
 
 
 class Seller(threading.Thread):
@@ -43,21 +38,16 @@ class Seller(threading.Thread):
         logger.info('Seller started work')
 
     def run(self):
-        global TOTAL_TICKETS
-        global TICKETS_MAX
+        global TOTAL_TICKETS, TICKETS_MAX
         is_running = True
         while is_running:
             self.random_sleep()
-            if TICKETS_MAX != 0 and TOTAL_TICKETS <= 4:
-                continue
-            else:
-                if TOTAL_TICKETS == 0:
+            with self.sem:
+                if TICKETS_MAX <= 0:
                     break
-                with self.sem:
-                    self.tickets_sold += 1
-                    TOTAL_TICKETS -= 1
-                    logger.info(f'{self.name} sold one;  {TOTAL_TICKETS} left')
-                    logger.info(f'{self.name} MAX_TICKETS = {TICKETS_MAX}')
+                self.tickets_sold += 1
+                TICKETS_MAX -= 1
+                logger.info(f'{self.name} sold one;  {TICKETS_MAX} left')
         logger.info(f'Seller {self.name} sold {self.tickets_sold} tickets')
 
     def random_sleep(self):
@@ -66,20 +56,19 @@ class Seller(threading.Thread):
 
 def main():
     semaphore = threading.Semaphore()
-    workers = []
+
 
     director = Director(semaphore)
     director.start()
+    sellers = [director]
 
     for _ in range(4):
         seller = Seller(semaphore)
         seller.start()
-        workers.append(seller)
+        sellers.append(seller)
 
-    workers.append(director)
-
-    for worker in workers:
-        worker.join()
+    for seller in sellers:
+        seller.join()
 
 
 if __name__ == '__main__':
